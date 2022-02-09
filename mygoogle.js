e3d7d90e62757862995315244cf4831054baeae3
Lars Hinrichs <hinrichs.lars@gmail.com>
2022-02-09 13:27:22 -0600

    complete google js script

--- mygoogle.js
+++ mygoogle.js
@@ -1,3 +1,4 @@
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-GV89SH3H9Y"></script>
 <script>
 window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
